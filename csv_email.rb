5e68954394f2cf0d8aa4c91d1ec5d72f4728d9ae
Deon Heyns <heynsd@gmail.com>
2014-07-22 17:44:50 -0300

    Refactoring csv_email.rb to not assign to message variable in a loop

--- csv_email.rb
+++ csv_email.rb
@@ -3,24 +3,24 @@ require 'net/smtp'
 addresses = File.read('addresses.csv').split(',').map(&:strip)
 
 from = 'heynsd@gmail.com'
+password = 'a_password'
 
-smtp = Net::SMTP.new('smtp.gmail.com', 587)
-smtp.enable_starttls
-
-smtp.start(Socket.gethostname, "#{from}", 'a_password', :login) do |smtp|
-
-	addresses.each do |to|
-		message = <<EOM
+message = <<EOM
 From: Someone <#{from}>
-To: Someone2 <#{to}>
+To: Someone2 <%{to}>
 Subject: This is from Ruby
 Date: #{Time.now.strftime("%d/%m/%Y %H:%M")}
 
 This email was sent using Ruby
 EOM
-	
+
+smtp = Net::SMTP.new('smtp.gmail.com', 587)
+smtp.enable_starttls
+
+smtp.start(Socket.gethostname, "#{from}", "#{password}", :login) do |smtp|
+	addresses.each do |to|	
 		begin
-			smtp.send_message(message, "#{from}", "#{to}")
+			smtp.send_message(message % {to: to}, "#{from}", "#{to}")
 		rescue Exception => e
 			puts e
 		end
